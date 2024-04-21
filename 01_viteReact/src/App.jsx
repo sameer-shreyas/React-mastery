df35645fda3547525d51b6afc9020089923215f3
Sameer <sameershreyas13@gmail.com>
2024-04-21 16:34:40 +0530

    Basics

--- 01_viteReact/src/App.jsx
+++ 01_viteReact/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Chai from "./chai"
 function App() {
 
   return (
+    <>
     <h1>Chai aur React</h1>
+    <Chai />
+    </>
   )
 }
 
--- 01basics/src/App.js
+++ 01basics/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from "./Chai.js"
 function App() {
   return (
+    <>
     <h1>Chai aur React </h1>
+    <Chai />
+    </>
   );
 }
 
