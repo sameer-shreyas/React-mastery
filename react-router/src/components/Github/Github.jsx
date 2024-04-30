294f7e07f9b6e5ad8b7d90970a415a45590d526f
Sameer <sameershreyas13@gmail.com>
2024-04-30 17:44:47 +0530

    react-router-dom

---
+++ react-router/src/components/Github/Github.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/varuno8')
+    //     .then((res) => res.json())
+    //     .then((data) =>{
+    //         console.log(data);
+    //         setData(data)
+    //     } )
+    // }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.followers}
+        <img src={data.avatar_url} alt="Git Picture" width={300}></img>
+    </div>
+  )
+}
+
+export default Github
+export const githubInforLoader = async() => {
+    const response = await fetch('https://api.github.com/users/varuno8')
+    return response.json()
+}
